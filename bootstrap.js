ecdb7c3f5d10ef04819802013cc4cb6f5f19b455
Jeffrey van Hoven <wolferine@live.se>
2016-05-18 17:07:53 +0200

    Added livereload for electron

--- bootstrap.js
+++ bootstrap.js
@@ -2,5 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 class Ember {
-    
+    constructor() {
+        alert("Hello world!");
+    }
 }
